f24e9e4472244e36df02234910f244bebfcef5c3
Richard10454 <richard2512006@gmail.com>
2025-10-26 18:22:45 -0400

    Cambios de servidor 3

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import colors from "colors";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import morgan from "morgan";
 import swaggerUI from "swagger-ui-express";
 import swaggerSpec from "./config/swagger";
@@ -9,35 +9,49 @@ import db from "./config/db";
 
 console.log("BOOT:");
 
-// ------- Conexión a la base de datos -------
+// Conectar a base de datos
 async function connectDB() {
   try {
     await db.authenticate();
-    await db.sync();
-    console.log(colors.magenta("Base de datos conectada ✔"));
+    db.sync();
+    //console.log(colors.magenta("Base de datos conectada aaaa"));
   } catch (error) {
+    //console.log(error);
     console.log(colors.bgRed.white("Hubo un error al conectar a la BD"));
   }
 }
+
 connectDB();
 
+// Instacia de express
 const server = express();
 
-server.use(cors());
-server.options("*", cors());
+// Permitir conexiones
 
-// ------- Middleware -------
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de cors"));
+    }
+  },
+};
+
+server.use(cors(corsOptions));
+
+// Leer datos de formularios
 server.use(express.json());
+
 server.use(morgan("dev"));
 
-// ------- Rutas -------
 server.use("/api", Cafeteria);
 
-server.get("/api", (_req, res) => {
-  res.json({ ok: true, msg: "Desde API" });
+server.get("/api", (req, res) => {
+  res.json("Desde API");
 });
 
-// ------- Swagger -------
+//Docs
 server.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 export default server;
